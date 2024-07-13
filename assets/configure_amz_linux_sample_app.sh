2438aac936f6d37fa843d867e0c19b6e0568bf77
777marc@gmail.com <777marc@gmail.com>
2024-07-13 14:00:40 -0400

    update linux setup

--- assets/configure_amz_linux_sample_app.sh
+++ assets/configure_amz_linux_sample_app.sh
@@ -1,14 +1,25 @@
-#!/bin/bash -xe
-# Install OS packages
-yum update -y
-yum groupinstall -y "Development Tools"
-amazon-linux-extras install -y nginx1
-yum install -y nginx python3.11 python3.11-pip python3.11-devel ruby wget
-python3.11 -m pip install pipenv wheel
-python3.11 -m pip install uwsgi
+#!/usr/bin/env zsh
 
-# Code Deploy Agent
-cd /home/ec2-user
-wget https://aws-codedeploy-us-west-2.s3.us-west-2.amazonaws.com/latest/install
-chmod +x ./install
-./install auto
+# https://stackoverflow.com/questions/11542846/nvm-node-js-recommended-install-for-all-users
+echo "=================================N=O=D=E========================================"
+
+cd /usr/local/bin || exit 90
+
+git clone https://github.com/nvm-sh/nvm.git .nvm
+
+\. "/usr/local/bin/.nvm/nvm.sh"
+
+nvm install --lts
+
+node -e "console.log('Running Node.js ' + process.version)"
+
+cat << "EOF" > /etc/profile.d/npm.sh
+#!/usr/bin/env bash
+export NVM_DIR="/usr/local/bin/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm'}
+
+EOF
+
+chmod 755 /etc/profile.d/npm.sh
+
+npm install -g npm
